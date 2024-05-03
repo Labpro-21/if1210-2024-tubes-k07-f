761bc3b464864654cbd21bbdf20e8e8cab88e4d4
Theo Kurniady <19623077@std.stei.itb.ac.id>
2024-05-03 15:30:56 +0700

    fix: inventory interface and error message

--- src/inventories.py
+++ src/inventories.py
@@ -65,52 +65,70 @@ mons = read_csv(mons_path)
 currentUser = [3, 'Agen_P', 'platypus123', 'agent', 0] # cuma sampel, nanti diambil dari fungsi login
 
 def Inventory(currentUser, mInv, iInv, mons):
-  print(f"============ INVENTORY LIST (User ID: {currentUser[0]}) ============")
-  print(f"Jumlah O.W.C.A. Coin-mu sekarang {currentUser[4]}")
-  invCount = 0
-  mTemp = []
-  iTemp = []
+    print(f"============ INVENTORY LIST (User ID: {currentUser[0]}) ============")
+    print(f"Jumlah O.W.C.A. Coin-mu sekarang {currentUser[4]}")
+    invCount = 0
+    mTemp = []
+    iTemp = []
 
+    for i in range(len(mInv)):
+        if mInv[i][0] == str(currentUser[0]):
+            print(
+                f"{invCount+1}. Monster       (Name: {mons[i][1]}, Lvl: {mInv[i][2]}, HP: {mons[i][4]})")
+            invCount += 1
+            stats = [invCount, mons[i][1], mons[i][2],
+                    mons[i][3], mons[i][4], mInv[i][2]]
+            mTemp.append(stats)
 
-  for i in range(len(mInv)):
-      if mInv[i][0] == str(currentUser[0]):
-          print(
-              f"{invCount+1}. Monster       (Name: {mons[i][1]}, Lvl: {mInv[i][2]}, HP: {mons[i][4]})")
-          invCount += 1
-          stats = [invCount, mons[i][1], mons[i][2],
-                  mons[i][3], mons[i][4], mInv[i][2]]
-          mTemp.append(stats)
+    for i in range(len(iInv)):
+        if iInv[i][0] == str(currentUser[0]):
+            print(
+                f"{invCount+1}. Potion        (Type: {iInv[i][1]}, Qty: {iInv[i][2]})")
+            invCount += 1
+            potInfo = [invCount, iInv[i][1], iInv[i][2]]
+            iTemp.append(potInfo)
+        
+    if len(mTemp) == 0 and len(iTemp) == 0:
+        inventoryBool = False
+    else:
+        inventoryBool = True
+    print()
 
-  for i in range(len(iInv)):
-      if iInv[i][0] == str(currentUser[0]):
-          print(
-              f"{invCount+1}. Potion        (Type: {iInv[i][1]}, Qty: {iInv[i][2]})")
-          invCount += 1
-          potInfo = [invCount, iInv[i][1], iInv[i][2]]
-          iTemp.append(potInfo)
-
-  print()
-  while True:
-      pilihanInv = input(">>> ")
-      if pilihanInv.upper() != "KELUAR":
-          if int(pilihanInv) <= int(len(mTemp)):
-              for i in range(len(mTemp)):
-                  if int(pilihanInv) == mTemp[i][0]:
-                      print("Monster")
-                      print(f"Name      : {mTemp[i][1]}")
-                      print(f"ATK Power : {mTemp[i][2]}")
-                      print(f"Def Power : {mTemp[i][3]}")
-                      print(f"HP        : {mTemp[i][4]}")
-                      print(f"Level     : {mTemp[i][5]}")
-                      break
-          elif int(pilihanInv) > int(len(mTemp)):
-              for i in range(len(iTemp)):
-                  if int(pilihanInv) == iTemp[i][0]:
-                      print("Potion")
-                      print(f"Type      : {iTemp[i][1]}")
-                      print(f"Quantity  : {iTemp[i][2]}")
-                      break
-      elif pilihanInv.upper() == "KELUAR":
-          break
+    if inventoryBool:
+        while True:
+            print ("Masukkan pilihan sesuai angka diatas atau 'KELUAR'")
+            pilihanInv = input(">> ")
+            if pilihanInv == "":
+                pass
+            elif pilihanInv.upper() != "KELUAR":
+                if invCount == 1 and pilihanInv > 1:
+                    print ("Pilihan hanyalah 1 dan 'KELUAR'")
+                    print ()
+                elif int(pilihanInv) > invCount:
+                    print (f"Pilihan hanyalah 1-{invCount} dan 'KELUAR'")
+                    print()
+                elif int(pilihanInv) <= int(len(mTemp)):
+                    for i in range(len(mTemp)):
+                        if int(pilihanInv) == mTemp[i][0]:
+                            print("Monster")
+                            print(f"Name      : {mTemp[i][1]}")
+                            print(f"ATK Power : {mTemp[i][2]}")
+                            print(f"Def Power : {mTemp[i][3]}")
+                            print(f"HP        : {mTemp[i][4]}")
+                            print(f"Level     : {mTemp[i][5]}")
+                            print()
+                            break
+                elif int(pilihanInv) > int(len(mTemp)):
+                    for i in range(len(iTemp)):
+                        if int(pilihanInv) == iTemp[i][0]:
+                            print("Potion")
+                            print(f"Type      : {iTemp[i][1]}")
+                            print(f"Quantity  : {iTemp[i][2]}")
+                            print()
+                            break
+            elif pilihanInv.upper() == "KELUAR":
+                print()
+                break
+            
 
 Inventory(currentUser, mInv, iInv, mons)
