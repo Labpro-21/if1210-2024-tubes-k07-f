4e4e72c4b1e7e3a78962691436f16ae2e7b265f8
Theo Kurniady <19623077@std.stei.itb.ac.id>
2024-05-07 23:40:34 +0700

    feat: inventory

--- src/inventories.py
+++ src/inventories.py
@@ -1,71 +1,38 @@
+from src.login import *
 import os
 
-def read_csv(file_path):
-    data = []
-    with open(file_path, 'r') as file:
-        row = []
-        field = ""
-        in_quotes = False
-
-        for char in file.read():
-            if char == ';' and not in_quotes:
-                row.append(field)
-                field = ""
-            elif char == '"' and not in_quotes:
-                in_quotes = True
-            elif char == '"' and in_quotes:
-                in_quotes = False
-            elif char == '\n' and not in_quotes:
-                row.append(field)
-                data.append(row)
-                row = []
-                field = ""
-            else:
-                field += char
-
-        if field:
-            row.append(field)
-            data.append(row)
-
-    return data
+from src.csv import *
+mons = read_csv(monster_filepath())
+mInv = read_csv(monster_inventory_filepath())
+iInv = read_csv(item_inventory_filepath())
 
 
-def get_current_directory():
-    return os.path.dirname(os.path.abspath(__file__))
+def mInvList():
+    for i in range(len(mInv)):
+        if mInv[i][0] == str(currentUser[0]):
+            print(
+                f"{invCount+1}. Monster       (Name: {mons[i][1]}, Lvl: {mInv[i][2]}, HP: {mons[i][4]})")
+            invCount += 1
+            stats = [invCount, mons[i][1], mons[i][2],
+                     mons[i][3], mons[i][4], mInv[i][2]]
+            mTemp.append(stats)
+    return mTemp
 
-tempFilepath = get_current_directory()
-charFilepath = [char for char in tempFilepath]
-newcharFilepath = []
-tempFilepath = get_current_directory()
-charFilepath = [char for char in tempFilepath]
-newcharFilepath = []
 
-if charFilepath[-1] == "c" and charFilepath[-2] == "r" and charFilepath[-3] == "s" and charFilepath[-4] == "\\":
-    for i in range(len(charFilepath)-4):
-        if ord(charFilepath[i]) != 92:
-            newcharFilepath.append(charFilepath[i])
-        else:
-            newcharFilepath.append("/")
-else:
-    for i in (charFilepath):
-        if ord(i) != 92:
-            newcharFilepath.append(i)
-        else:
-            newcharFilepath.append("/")
+def iInvList():
+    for i in range(len(iInv)):
+        if iInv[i][0] == str(currentUser[0]):
+            print(
+                f"{invCount+1}. Potion        (Type: {iInv[i][1]}, Qty: {iInv[i][2]})")
+            invCount += 1
+            potInfo = [invCount, iInv[i][1], iInv[i][2]]
+            iTemp.append(potInfo)
+    return iTemp
 
-filepath = ("".join(map(str, newcharFilepath)))
-mInv_path = filepath+"/data/monster_inventory.csv"
-iInv_path = filepath+"/data/item_inventory.csv"
-mons_path = filepath+"/data/monster.csv"
 
-mInv = read_csv(mInv_path)
-iInv = read_csv(iInv_path)
-mons = read_csv(mons_path)
-
-currentUser = [3, 'Agen_P', 'platypus123', 'agent', 0] # cuma sampel, nanti diambil dari fungsi login
-
-def Inventory(currentUser, mInv, iInv, mons):
-    print(f"============ INVENTORY LIST (User ID: {currentUser[0]}) ============")
+def INVENTORY(currentUser, mInv, iInv, mons):
+    print(
+        f"============ INVENTORY LIST (User ID: {currentUser[0]}) ============")
     print(f"Jumlah O.W.C.A. Coin-mu sekarang {currentUser[4]}")
     invCount = 0
     mTemp = []
@@ -77,7 +44,7 @@ def Inventory(currentUser, mInv, iInv, mons):
                 f"{invCount+1}. Monster       (Name: {mons[i][1]}, Lvl: {mInv[i][2]}, HP: {mons[i][4]})")
             invCount += 1
             stats = [invCount, mons[i][1], mons[i][2],
-                    mons[i][3], mons[i][4], mInv[i][2]]
+                     mons[i][3], mons[i][4], mInv[i][2]]
             mTemp.append(stats)
 
     for i in range(len(iInv)):
@@ -87,7 +54,7 @@ def Inventory(currentUser, mInv, iInv, mons):
             invCount += 1
             potInfo = [invCount, iInv[i][1], iInv[i][2]]
             iTemp.append(potInfo)
-        
+
     if len(mTemp) == 0 and len(iTemp) == 0:
         inventoryBool = False
     else:
@@ -96,16 +63,17 @@ def Inventory(currentUser, mInv, iInv, mons):
 
     if inventoryBool:
         while True:
-            print ("Masukkan pilihan sesuai angka diatas atau 'KELUAR'")
+            print("Masukkan pilihan sesuai angka diatas atau 'KELUAR'")
             pilihanInv = input(">> ")
-            if pilihanInv == "":
-                pass
-            elif pilihanInv.upper() != "KELUAR":
-                if invCount == 1 and pilihanInv > 1:
-                    print ("Pilihan hanyalah 1 dan 'KELUAR'")
-                    print ()
+            try:
+                pilihanInv = int(pilihanInv)
+                if pilihanInv == 0:
+                    print()
+                elif invCount == 1 and pilihanInv > 1:
+                    print("Pilihan hanyalah 1 dan 'KELUAR'")
+                    print()
                 elif int(pilihanInv) > invCount:
-                    print (f"Pilihan hanyalah 1-{invCount} dan 'KELUAR'")
+                    print(f"Pilihan hanyalah 1-{invCount} dan 'KELUAR'")
                     print()
                 elif int(pilihanInv) <= int(len(mTemp)):
                     for i in range(len(mTemp)):
@@ -126,9 +94,11 @@ def Inventory(currentUser, mInv, iInv, mons):
                             print(f"Quantity  : {iTemp[i][2]}")
                             print()
                             break
-            elif pilihanInv.upper() == "KELUAR":
-                print()
-                break
-            
+            except ValueError:
+                if pilihanInv.upper() == "KELUAR":
+                    print()
+                    break
+                else:
+                    print()
 
-Inventory(currentUser, mInv, iInv, mons)
+# INVENTORY(currentUser, mInv, iInv, mons)
