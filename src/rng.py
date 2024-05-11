6b7efd5f28c67820023f4998a6de918e3cf8dbe0
Theo Kurniady <19623077@std.stei.itb.ac.id>
2024-05-11 21:54:22 +0700

    feat: add rng for level, enemy, and OC

---
+++ src/rng.py
@@ -0,0 +1,51 @@
+import time
+from csv import *
+
+class LCG:
+    def __init__(self, min_val, max_val):
+        self.m = 2**32
+        self.a = 1664525
+        self.c = 1013904223
+        self.seed = int(time.time())  # Initialize seed with current time
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def set_seed(self, seed):
+        self.seed = seed
+
+    def generate(self):
+        self.seed = (self.a * self.seed + self.c) % self.m
+        scaled_number = self.min_val + \
+            (self.seed / self.m) * (self.max_val - self.min_val)
+        return int(scaled_number)  # Convert the scaled number to an integer
+
+
+# Example usage
+def rngLevel(LCG):
+    lcgList = []
+    lcg = LCG(1, 6)  # Generate integers between 0 and 100
+    for _ in range(11):
+        lcgList.append(lcg.generate())
+
+    rng = (lcgList[5])
+    return rng
+
+def rngEnemy(LCG, mons):
+    lcgList = []
+    lcg = LCG(1, len(mons))  # Generate integers between 0 and 100
+    for _ in range(11):
+        lcgList.append(lcg.generate())
+
+    rng = (lcgList[5])
+    return rng
+    
+def rngOC(LCG):
+    lcgList = []
+    lcg = LCG(5, 30)  # Generate integers between 0 and 100
+    for _ in range(11):
+        lcgList.append(lcg.generate())
+
+    rng = (lcgList[5])
+    return rng
+
+# rng = (rng(LCG))
