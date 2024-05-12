43554fe7742f38fc8818ee2cd41470fdb31ad255
Theo Kurniady <19623077@std.stei.itb.ac.id>
2024-05-12 13:49:42 +0700

    fix: defense and healing limit cap calculations

--- src/monster.py
+++ src/monster.py
@@ -4,6 +4,9 @@ def monster(atk_power:int, def_power:int, hp:int, level:int):
         atk_power *= add
         def_power *= add
         hp *= add
+    if def_power > 50:
+        def_power = 50
+
     return int(atk_power), int(def_power), int(hp)
 
 #print(monster)
