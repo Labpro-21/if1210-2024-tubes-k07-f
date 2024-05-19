623fbee8a5ca7c63a1d38714748fee8e909c1689
Theo Kurniady <19623077@std.stei.itb.ac.id>
2024-05-20 00:52:26 +0700

    fix: max upgrade error and invalid monster number

--- src/laboratory.py
+++ src/laboratory.py
@@ -30,52 +30,50 @@ def upgrade_monster(currency, monsters, currentUser, mons, mInv, userpas, is_int
     if is_integer(monster_choice):
         monster_choice = int(monster_choice) - 1
 
-        if monster_choice < 0 or monster_choice >= len(monsters):
+        if (monster_choice < 0) or (monster_choice >= len(monsters)):
             print("Pilihan monster tidak valid.")
-            return
-
-        monster = monsters[monster_choice]
-        print(monster)
-        if monster[1] >= 5:
-            print("Maaf, monster sudah mencapai level maksimum.")
-            return
-
-        upgrade_price = upgrade_prices.get(monster[1] + 1)
-
-        print(f"{monster[0]} akan di-upgrade ke level {monster[1] + 1}.")
-        print(
-            f"Harga untuk melakukan upgrade {monster[0]} adalah {upgrade_price} OC.")
-
-        confirm = input(">>> Lanjutkan upgrade (Y/N): ").upper()
-        if confirm == "Y":
-            if currency >= upgrade_price:
-                currency -= upgrade_price
-                currentUser[4] = int(currentUser[4])
-                currentUser[4] = currency
-                for i in range(1, len(userpas)):
-                    if userpas[i][0] == currentUser[0]:
-                        userpas[i][4] = int(userpas[i][4])
-                        userpas[i][4] = currency
-
-                monster[1] += 1
-                breakout = False
-                for i in range(1, len(mInv)):
-                    for j in range(1, len(mons)):
-                        if (str(mInv[i][0]) == str(currentUser[0])):
-                            if (mons[j][1] == monster[0]):
-                                if str(mons[j][0]) == str(mInv[i][1]):
-                                    mInv[i][2] = int(mInv[i][2])
-                                    mInv[i][2] += 1
-                                    breakout = True
-                                    break
-                    if breakout:
-                        break
-                print(
-                    f"Selamat, {monster[0]} berhasil di-upgrade ke level {monster[1]}!")
-            else:
-                print("Maaf, Anda tidak memiliki cukup OC untuk melakukan upgrade.")
+            
         else:
-            print("Upgrade dibatalkan.")
+            monster = monsters[monster_choice]
+            if monster[1] >= 5:
+                print("Maaf, monster sudah mencapai level maksimum.")
+
+            else:
+                upgrade_price = upgrade_prices.get(monster[1] + 1)
+                print(f"{monster[0]} akan di-upgrade ke level {monster[1] + 1}.")
+                print(
+                    f"Harga untuk melakukan upgrade {monster[0]} adalah {upgrade_price} OC.")
+
+                confirm = input(">>> Lanjutkan upgrade (Y/N): ").upper()
+                if confirm == "Y":
+                    if currency >= upgrade_price:
+                        currency -= upgrade_price
+                        currentUser[4] = int(currentUser[4])
+                        currentUser[4] = currency
+                        for i in range(1, len(userpas)):
+                            if userpas[i][0] == currentUser[0]:
+                                userpas[i][4] = int(userpas[i][4])
+                                userpas[i][4] = currency
+
+                        monster[1] += 1
+                        breakout = False
+                        for i in range(1, len(mInv)):
+                            for j in range(1, len(mons)):
+                                if (str(mInv[i][0]) == str(currentUser[0])):
+                                    if (mons[j][1] == monster[0]):
+                                        if str(mons[j][0]) == str(mInv[i][1]):
+                                            mInv[i][2] = int(mInv[i][2])
+                                            mInv[i][2] += 1
+                                            breakout = True
+                                            break
+                            if breakout:
+                                break
+                        print(
+                            f"Selamat, {monster[0]} berhasil di-upgrade ke level {monster[1]}!")
+                    else:
+                        print("Maaf, Anda tidak memiliki cukup OC untuk melakukan upgrade.")
+                else:
+                    print("Upgrade dibatalkan.")
     else:
         print("Masukkan angka yang valid!!")
     return currency, userpas, mInv
