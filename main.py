5e92429533e80e43fd16a98a0b3c646c79a99a85
Theo Kurniady <19623077@std.stei.itb.ac.id>
2024-05-19 17:11:32 +0700

    feat: add function int validation

--- main.py
+++ main.py
@@ -25,11 +25,20 @@ from src.laboratory import *
 from src.save import *
 from src.load import *
 
+# Fungsi validasi input berbentuk integer atau tidak
+def is_integer(input):
+    is_integer = True
+    for char in input:
+        # Mengecek input menggunakan ascii number (0-9 : 48-57)
+        if not (48 <= ord(char) <= 57):
+            is_integer = False
+    return is_integer
+
 userpas, mons, iInv, mInv, iShop, mShop, valid_load = start()
 print("Selamat datang di program OWCA!")
 print("Ketik HELP untuk memunculkan bantuan dari The Mighty God.")
 print()
-
+print(mShop)
 login = False
 while True:
     pilihan = input(">>> ")
