dde406e25c734078305776479b5d874bdb9d92d5
Theo Kurniady <19623077@std.stei.itb.ac.id>
2024-05-20 09:39:30 +0700

    fix: int validation

--- src/shopcurrency.py
+++ src/shopcurrency.py
@@ -1,8 +1,6 @@
-from src.login import *
 import os
 
-from src.register import *
-from src.inventories import *
+from src.isInteger import *
 
 
 def mShopList(shopCount, currentUser, mShop, mons, msTemp):
@@ -63,58 +61,73 @@ def SHOP (currentUser, mShop, iShop, mons, mInv, iInv) :
             print ()
             beli = str(input("Mau beli apa? (monster/item): "))
             if beli == "monster" :
-                id_monster = int(input("Masukkan id monster: "))
-                if id_monster > (len(mShop)-1) : 
-                    print ("Pilihan monster tidak valid.")
-                    print ()
-                else :
-                    if mShop [id_monster][1] == 0 :
-                        print ("Pembelian gagal, stok monster kurang.")
+                id_monster = (input("Masukkan id monster: "))
+                if is_integer(id_monster):
+                    id_monster = int(id_monster)
+                    if id_monster > (len(mShop)-1) : 
+                        print ("Pilihan monster tidak valid.")
                         print ()
-                    elif int(mShop [id_monster][2]) > currentUser[4] :
-                        print ("OC-mu tidak cukup.")
-                        print ()
-                    elif cekMonster (mShop, mInv, id_monster, currentUser) :
-                        print (f"Monster {mons[int(mShop[id_monster][0])][1]} sudah ada dalam inventory-mu! Pembelian dibatalkan.")
                     else :
-                        print (f"Berhasil membeli item: {mons[int(mShop[id_monster][0])][1]}. Item sudah masuk ke inventory-mu!")
-                        print ()
-                        currentUser[4] -= int(mShop [id_monster][2])
-                        mShop [id_monster][1] = int(mShop [id_monster][1]) - 1
-                        mons_to_inv = [currentUser [0], mons[int(mShop[id_monster][0])][0], 1]
-                        mInv.append (mons_to_inv)
+                        if mShop [id_monster][1] == 0 :
+                            print ("Pembelian gagal, stok monster kurang.")
+                            print ()
+                        elif int(mShop [id_monster][2]) > currentUser[4] :
+                            print ("OC-mu tidak cukup.")
+                            print ()
+                        elif cekMonster (mShop, mInv, id_monster, currentUser) :
+                            print (f"Monster {mons[int(mShop[id_monster][0])][1]} sudah ada dalam inventory-mu! Pembelian dibatalkan.")
+                        else :
+                            print (f"Berhasil membeli item: {mons[int(mShop[id_monster][0])][1]}. Item sudah masuk ke inventory-mu!")
+                            print ()
+                            currentUser[4] -= int(mShop [id_monster][2])
+                            mShop [id_monster][1] = int(mShop [id_monster][1]) - 1
+                            mons_to_inv = [currentUser [0], mons[int(mShop[id_monster][0])][0], 1]
+                            mInv.append (mons_to_inv)
+                else:
+                    print("Input harus berupa integer.")
+                    print()
             
             elif beli == "item" :
-                id_item = int(input("Masukkan id item: "))
-                jumlah_item = int(input("Masukkan jumlah item: "))
-                if id_item > len(iShop)-1 : 
-                    print ("Pilihan item tidak valid.")
-                    print ()
-                else :
-                    itemCek = cekItem (iShop, iInv, id_item, currentUser) 
-                    if int(iShop [id_item][1]) < jumlah_item :
-                        print ("Pembelian gagal, stok item kurang.")
-                        print ()
-                    elif int(iShop [id_item][2])*jumlah_item > currentUser[4] :
-                        print ("OC-mu tidak cukup.")
-                        print ()
-                    elif itemCek:
-                        print(f"Berhasil membeli item: {jumlah_item} {iShop [id_item][0]}. Item sudah masuk ke inventory-mu!")
+                id_item = (input("Masukkan id item: "))
+                if is_integer(id_item):
+                    id_item = int(id_item)
+                    jumlah_item = (input("Masukkan jumlah item: "))
+                    if is_integer(jumlah_item):
+                        jumlah_item = int(jumlah_item)
+                        if id_item > len(iShop)-1 : 
+                            print ("Pilihan item tidak valid.")
+                            print ()
+                        else :
+                            itemCek = cekItem (iShop, iInv, id_item, currentUser) 
+                            if int(iShop [id_item][1]) < jumlah_item :
+                                print ("Pembelian gagal, stok item kurang.")
+                                print ()
+                            elif int(iShop [id_item][2])*jumlah_item > currentUser[4] :
+                                print ("OC-mu tidak cukup.")
+                                print ()
+                            elif itemCek:
+                                print(f"Berhasil membeli item: {jumlah_item} {iShop [id_item][0]}. Item sudah masuk ke inventory-mu!")
+                                print()
+                                currentUser [4] -= int(iShop [id_item][2])*jumlah_item
+                                iShop [id_item][1] = int(iShop [id_item][1]) - jumlah_item
+                                item_to_inv = [currentUser[0], iShop [id_item][0], jumlah_item]
+                                iInv.append (item_to_inv)
+                                isTemp.clear()
+                            else :
+                                print(f"Berhasil membeli item: {jumlah_item} {iShop [id_item][0]}. Item sudah masuk ke inventory-mu!")
+                                print()
+                                currentUser [4] -= int(iShop [id_item][2])*jumlah_item
+                                iShop [id_item][1] = int(iShop [id_item][1]) - jumlah_item
+                                for i in range (len(iInv)) :
+                                    if str(currentUser[0]) == str(iInv[i][0]):
+                                        if iShop [id_item][0] == iInv [i][1] :
+                                            iInv[i][2] = int(iInv[i][2])
+                                            iInv[i][2] += jumlah_item
+                    else:
+                        print("Input harus berupa integer")
                         print()
-                        currentUser [4] -= int(iShop [id_item][2])*jumlah_item
-                        iShop [id_item][1] = int(iShop [id_item][1]) - jumlah_item
-                        item_to_inv = [currentUser[0], iShop [id_item][0], jumlah_item]
-                        iInv.append (item_to_inv)
-                        isTemp.clear()
-                    else :
-                        print(f"Berhasil membeli item: {jumlah_item} {iShop [id_item][0]}. Item sudah masuk ke inventory-mu!")
-                        print()
-                        currentUser [4] -= int(iShop [id_item][2])*jumlah_item
-                        iShop [id_item][1] = int(iShop [id_item][1]) - jumlah_item
-                        for i in range (len(iInv)) :
-                            if str(currentUser[0]) == str(iInv[i][0]):
-                                if iShop [id_item][0] == iInv [i][1] :
-                                    iInv[i][2] = int(iInv[i][2])
-                                    iInv[i][2] += jumlah_item
+                else:
+                    print("Input harus berupa integer")
+                    print()
 
     print("Mr. Yanto bilang makasih, belanja lagi ya nanti!!!")
